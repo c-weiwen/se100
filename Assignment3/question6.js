268a280f067f35ed888ba8863489ac43a533e97e
c-weiwen <weiwen@outlook.sg>
2025-05-25 19:23:51 +0800

    c

--- Assignment3/question6.js
+++ Assignment3/question6.js
@@ -7,3 +7,22 @@ const disneyData = {
 };
 
 // Write your code below
+const open = parseFloat(disneyData.open);
+const close = parseFloat(disneyData.close);
+const volume = parseInt(disneyData.volume);
+
+if (close > open) {
+	if (volume > 100000) {
+		console.log('Strong Bullish');
+	} else {
+		console.log('Bullish');
+	}
+} else if (close < open) {
+	if (volume > 10000) {
+		console.log('Strong Bearish');
+	} else {
+		console.log('Bearish');
+	}
+} else {
+	console.log('neutral');
+}
